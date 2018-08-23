3dc1d4c84e4c1396c6fa9d17de103c6ea15c4f81
Gavin Morris <gavin@almostpurple.com>
2018-08-23 10:57:47 +0800

    add lfs working dir cleanup

--- init.sh
+++ init.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 local_repo=import-test
-remote_repo=import-test
+remote_repo=stormcrow79/import-test
 
+# reinit the repo
 cd /c/github/gitplay
 rm -rf $local_repo
 git init $local_repo
 
 # change into new repo
 cd $local_repo
+
+# init lfs
 git lfs install
 git lfs track *.dat
 git add .gitattributes
 git commit -m "init lfs"
-git remote add origin https://github.com/stormcrow79/$remote_repo.git
+
+# push empty repo to github
+git remote add origin https://github.com/$remote_repo.git
 git push -u origin master --force
 
+# get out
 #exit
 
 # prime lfs cache
@@ -31,4 +37,9 @@ cp ../data1.dat .git/lfs/objects/d6/f3/d6f34137c06643d2e6af7c36f02b215c2a67ce218
 cp ../data2.dat .git/lfs/objects/14/c9/14c90dddf6d5971aff9b54677363a485b5dc4d2d3cd0b5b835827a2b49d2e5c4
 cp ../data3.dat .git/lfs/objects/4b/20/4b2022fa0a54d83df1696d5351d28300f263dd776c14b608d14426e3081b07b7
 
+# run the import
 git fast-import < ../fast-lfs
+
+# reset the lfs files
+git reset HEAD .
+git checkout -- .
